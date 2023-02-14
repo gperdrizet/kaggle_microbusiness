d5af407e44df31f339cc4fd65e8da1cd8533f555
George Perdrizet <george@perdrizet.org>
2023-02-14 10:42:20 -0500

    Setup major refactor of bootstrapping functions and logic out of notebooks to modules to clean-up and refocus notebooks on analysis of results, not implementation.

---
+++ bootstrap_linear_models.py
@@ -0,0 +1,65 @@
+# Add parent directory to path to allow import of config.py
+import sys
+sys.path.append('..')
+import config as conf
+import functions.initialization_functions as init_funcs
+import functions.data_manipulation_functions as data_funcs
+import functions.parallization_functions as parallel_funcs
+
+# import time
+import numpy as np
+# import pandas as pd
+# import statsmodels.api as sm
+# from scipy import stats
+# from sklearn.linear_model import Ridge
+
+# print(f'Python: {sys.version}')
+# print()
+# print(f'Numpy {np.__version__}')
+# print(f'Pandas {pd.__version__}')
+
+# Load parsed data
+block_size = 5
+
+output_file = f'{conf.DATA_PATH}/parsed_data/structured_bootstrap_blocksize{block_size}.npy'
+timepoints = np.load(output_file)
+
+# print(f'Timepoints shape: {timepoints.shape}')
+# print()
+# print('Column types:')
+
+# for column in timepoints[0,0,0,0:]:
+#     print(f'\t{type(column)}')
+
+# print()
+# print(f'Example block:\n{timepoints[0,0,0:,]}')
+
+# Set run parameters
+num_samples = 180
+sample_size = 1500
+model_orders = [10,12,14,16,32]
+model_types = ['OLS', 'TS', 'Seigel', 'Ridge']
+time_fit = False
+
+# Fire up the pool
+pool, result_objects = parallel_funcs.start_multiprocessing_pool()
+
+# Loop on samples, assigning each to a different worker
+for sample_num in range(num_samples):
+
+    result = pool.apply_async(parallel_funcs.parallel_bootstrapped_smape,
+        args = (
+            timepoints, 
+            sample_num, 
+            sample_size, 
+            model_orders, 
+            model_types,
+            time_fit
+        )
+    )
+
+    # Add result to collection
+    result_objects.append(result)
+
+# Get and parse result objects, clean up pool
+data = parallel_funcs.cleanup_bootstrapping_multiprocessing_pool(pool, result_objects)
---
+++ functions/parallization_functions.py
@@ -0,0 +1,90 @@
+# Add parent directory to path to allow import of config.py
+import sys
+sys.path.append('..')
+import functions.bootstrapping_functions as bootstrap_funcs
+
+import numpy as np
+import pandas as pd
+import multiprocessing as mp
+
+def start_multiprocessing_pool():
+    # Instantiate multiprocessing pool to parallelize over folds
+    n_cpus = mp.cpu_count() - 2
+
+    print(f'Starting processes for {n_cpus} CPUs (available - 2)')
+
+    pool = mp.Pool(processes = n_cpus)
+
+    # Holder for result objects
+    result_objects = []
+
+    return pool, result_objects
+
+def parallel_bootstrapped_smape(
+    timepoints, 
+    sample_num, 
+    sample_size, 
+    model_orders, 
+    model_types,
+    time_fit = False
+):
+    
+    # Holder for sample results
+    data = {
+        'sample': [],
+        'model_type': [],
+        'model_order': [],
+        'SMAPE_values': [],
+        'detrended_SMAPE_values': [],
+        'MBD_predictions': [],
+        'detrended_MBD_predictions': [],
+        'MBD_inputs': [],
+        'detrended_MBD_inputs': [],
+        'MBD_actual': []
+    }
+
+    # Loop on model orders
+    for model_order in model_orders:
+        result = bootstrap_funcs.bootstrap_smape_scores(            
+            timepoints, 
+            sample_num, 
+            sample_size, 
+            model_order, 
+            model_types,
+            time_fit
+        )
+
+        # Add results for this order
+        for key, value in result.items():
+            data[key].extend(value)
+
+    return data
+
+def cleanup_bootstrapping_multiprocessing_pool(pool, result_objects):
+
+    # Collect results
+    results = [result.get() for result in result_objects]
+
+    # Holder for parsed sample results
+    data = {
+        'sample': [],
+        'model_type': [],
+        'model_order': [],
+        'SMAPE_values': [],
+        'detrended_SMAPE_values': [],
+        'MBD_predictions': [],
+        'detrended_MBD_predictions': [],
+        'MBD_inputs': [],
+        'detrended_MBD_inputs': [],
+        'MBD_actual': []
+    }
+
+    for result in results:
+        for key, value in result.items():
+            data[key].extend(value)
+
+    # Clean up
+    pool.close()
+    pool.join()
+
+    return data
