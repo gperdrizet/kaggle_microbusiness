43ff6ed5f27d69e76eec2d6925b44f74a399aa7d
George Perdrizet <george@perdrizet.org>
2023-02-14 10:15:15 -0500

    Added logging parameters.

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 '''Configuration file for hardcoding python variables.
 Used to store things like file paths, model hyperparameters etc.'''
@@ -8,6 +9,14 @@ PROJECT_NAME = 'godaddy-microbusiness-density-forecasting'
 # Get path to this config file so that we can define 
 # other paths relative to it
 PROJECT_ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
+
+# Logging stuff
+LOG_LEVEL = logging.INFO
+LOG_ROTATION_FREQUENCY = 'h'
+LOG_BACKUP_COUNT = 24
+LOG_FORMAT = '%(asctime)s:%(name)s:%(levelname)s - %(message)s'
+LOG_DIR = f'{BASE_DIR}/logs/'
+#DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
 
 # Data related files & paths
 DATA_PATH = f'{PROJECT_ROOT_PATH}/data'
