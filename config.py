e8e04016d5197c2125efda79c9bd0ee5a781f872
George Perdrizet <george@perdrizet.org>
2023-03-01 18:52:15 -0500

    Finish first run of minimal GRU based neural network.

--- config.py
+++ config.py
@@ -90,9 +90,18 @@ class GRU_model_parameters():
 
     # Run specific files
     log_file_name = 'GRU_hyperparameter_bootstrapping.log'
-    input_file_root_name = 'structured_bootstrap_blocksize'
+    input_file_root_name = 'updated_structured_bootstrap_blocksize'
     output_file_root_name = 'GRU_hyperparameter_bootstrapping'
 
     # Data related stuff
     input_data_type = 'microbusiness_density'
+    block_size = 13
+    forecast_horizon = 5
+    num_counties = 'all'
+    test_timepoints = 1
     training_split_fraction = 0.7
+
+    # Hyperparameters
+    GRU_units = 32
+    learning_rate = 0.0002
+    epochs = 25
